769855a0cc198bee9706c5461a1c865045064db6
Marcus Greene <mn.greene@icloud.com>
2022-07-21 08:27:02 -0400

    changes, added ticker add window and webhook

--- stock watcher.py
+++ stock watcher.py
@@ -7,18 +7,29 @@ import json
 import threading
 import time
 import datetime
+from tkinter import *
+from tkinter import ttk
 
 
-config = {"stocks" : [], "notifications":"", "rate": 1200}
+config = {"stocks" : [], "webhook":"", "rate": 1200, "notifications":{}}
 stocks=[]
 
-from tkinter import *
+try:
+    fn=open("config.json","r")
+except: 
+    print("Error: File does not appear to exist. Creating config file.")
+    fn = open("config.json","w")
+    json.dump(config,fn)
+else:
+    config = json.load(fn)
+    stocks = config['stocks']
 
 flag = False
 
+#sees if the market is open based on the date and time
 def isopen():
     return True
-    '''#0 = monday, 1 = tuesday, etc
+    #0 = monday, 1 = tuesday, etc
     day = datetime.datetime.today().weekday()
     if day < 5:
         #day is a weekday
@@ -27,8 +38,9 @@ def isopen():
         if (hour >= 9 and minute >= 30) or (hour > 9):
             if hour < 16:
                 return True
-    return False'''
+    return False
 
+#checks if the market is open, and if it is, wait the refresh rate and fetch new prices
 def fetch(monitor=1200):
     global flag
     print(3)
@@ -40,10 +52,13 @@ def fetch(monitor=1200):
             T.configure(state="disabled")
             print("market is open, fetching new prices")
             num = 0
-            while num < monitor:
+            while num < monitor and flag:
                 time.sleep(10)
                 num += 10
                 print("fetching new info in " + str(monitor-num) + " seconds")
+            if flag == False:
+                print("Monitor was stopped")
+                return
             print('fetching!')
             '''headers = {
             "X-RapidAPI-Key": "2b1ab8509cmsh1d250472e255ecbp1b3a8djsnac24acb5b68b",
@@ -57,33 +72,9 @@ def fetch(monitor=1200):
             response = json.loads(response.text)
             askprice = response["quoteResponse"]['result'][0]['ask']
             print(askprice)'''
-
-def notify(url):
-    hook = Webhook(url=url)
-    hook.send("test")
     
 def menu(config):
-    try:
-        fn=open("config.json","r")
-    except: 
-        print("Error: File does not appear to exist. Creating config file.")
-        fn = open("config.json","w")
-        json.dump(config,fn)
-    else:
-        config = json.load(fn)
-        stocks = config['stocks']
-
-    start = input("What would you like to do? Enter 'add' to add a ticker, 'notify' to set a price notification, 'remove' to remove a ticker, 'config' to edit the settings, or 'monitor' to begin monitoring prices")
-    if start == "add":
-        go = input("Enter your stock ticker")
-        try:
-            config['stocks'].append(go)
-        #if there is no stocks key, start one with this input as the first key in a list
-        except:
-            config['stocks'] = [go]
-        fn = open('config.json','w')
-        json.dump(config, fn)
-    elif start == "notify":
+    if start == "notify":
         sys = input("Enter the stock ticker you would like to watch")
         price = input("Enter the price target")
         ou = input("Over or under this price target?")
@@ -112,8 +103,7 @@ def menu(config):
             config["rate"] = 1200
         t = threading.Thread(target=fetch(rate))
     
-
-
+#triggered upon monitor button clicked
 def go():
     global flag
     if flag == True:
@@ -123,22 +113,62 @@ def go():
     print(1)
     fetch(1200)
 
+#adds the ticker to the stock list
+def addticker():
+    ticker = my_entry.get()
+    config['stocks'].append(ticker)
+    my_entry.delete(0,END)
+    fn = open("config.json","w")
+    json.dump(config,fn)
+    print(config['stocks'])
+    Combo['values'] = tuple(list(Combo['values']).append(ticker))
+    #TODO json dump
+    #TODO try and except block for wrong ticker
+
+def testhook():
+    hook = Webhook(url=webhook.get())
+    hook.send("test")
+    config['webhook'] = webhook.get()
+    fn = open("config.json","w")
+    json.dump(config,fn)
+
+def gui():
+    btn.pack()
+    T.pack()
+    my_entry.pack()
+    submit.pack()
+    webhook.pack()
+    test.pack()
+    Combo.pack()
+    stopbtn.pack()
+
+def unflag():
+    global flag
+    flag = False
+
 
 window=Tk()
 # add widgets here
 btn=Button(window, text="Monitor", fg='blue', command=threading.Thread(target=go, daemon=True).start)
-btn.pack()
+stopbtn = Button(window, text= "Stop Monitor", command= unflag)
 T = Text(window, height = 5, width = 52, state="disabled")
-T.pack()
+my_entry = Entry(window)
+submit = Button(window, text="submit", command=addticker)
+webhook = Entry(window)
+if len(config['webhook']) > 0:
+    webhook.insert(END,config["webhook"])
+test = Button(window, command = testhook, text="test webhook")
+Combo = ttk.Combobox(window, values = stocks)
+Combo.set("Pick an Option")
 window.title('Stock watcher')
 window.geometry("300x200+10+20")
 
+gui()
 window.mainloop()
+
 #t2 = threading.Thread(target= window.mainloop())
 #t2.start()
 
-#todo: add window
 #remove selector
-#webhook section
 #refresh rate section
 
